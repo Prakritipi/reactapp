ed13fbd2d9d87e5e28a956ea4cbc3390819fc953
prakritipi <prakritiacharyaa@gmail.com>
2025-06-02 16:08:44 +0545

    reinitialized git

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="container">
+      <header >
+        <h1>🌼 Welcome to My Landing Page   testing...</h1>
+        <p>This is my very first React app made using Vite!</p>
+        <button>Let's Begin</button>
+      </header>
+    </div>
+
+    // <div className='bg-red'>
+
+    //   this is react component
+    // </div>
+  );
+}
+
+export default App;
